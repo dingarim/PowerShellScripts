ec7215bb66c97c1aa5b551770ce3fa62be4b3a63
Murali Mohan Dingari <dingarim@users.noreply.github.com>
2020-04-15 09:44:03 -0500

    Update Enable-PSRemoting.ps1

--- Enable-PSRemoting.ps1
+++ Enable-PSRemoting.ps1
@@ -3,11 +3,7 @@ Enable-PSRemoting –force
 # Set start mode to automatic
 Set-Service WinRM -StartMode Automatic
 
-# Verify start mode and state - it should be running
-#Get-WmiObject -Class win32_service | Where-Object {$_.name -like "WinRM"}
 
 # Trust all hosts
 Set-Item WSMan:localhost\client\trustedhosts -value *
 
-# Verify trusted hosts configuration
-Get-Item WSMan:\localhost\Client\TrustedHosts
